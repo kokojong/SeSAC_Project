53f1e44c4ab097676bafc7fa3c15f2e5d4a93762
kokojong <duwhd0349@kookmin.ac.kr>
2021-10-23 23:42:25 +0900

    10.23(sat) autolayout exercise

---
+++ AutoLayout_assignment/AutoLayout_assignment/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AutoLayout_assignment
+//
+//  Created by kokojong on 2021/10/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
