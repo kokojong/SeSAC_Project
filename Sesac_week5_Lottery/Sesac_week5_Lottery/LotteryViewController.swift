603ffc2f2e7ea381554a0d48ea70a8faa57a98e8
kokojong <duwhd0349@kookmin.ac.kr>
2021-11-01 02:30:53 +0900

    10.31(sun) Lottery project UserDefaluts added

--- Sesac_week5_Lottery/Sesac_week5_Lottery/LotteryViewController.swift
+++ Sesac_week5_Lottery/Sesac_week5_Lottery/LotteryViewController.swift
@@ -27,6 +27,7 @@ class LotteryViewController: UIViewController {
     
     let numberList: [Int] = Array(1...986).reversed()
     
+    var savedArray: [Any] = []
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -47,42 +48,78 @@ class LotteryViewController: UIViewController {
     }
     
     func getLotteryInfo(drwNo: String) {
-            
-        let url = "https://www.dhlottery.co.kr/common.do?method=getLottoNumber&drwNo=\(drwNo)"
         
-            // https://www.dhlottery.co.kr/common.do? method=getLottoNumber&drwNo=903
+        if checkDrwNo(drwNo: drwNo){
+            // 이미 저장된 값이면
+            print("savedArray: \(savedArray)")
             
-            AF.request(url, method: .get).validate().responseJSON { response in
-                switch response.result {
-                case .success(let value):
-                    let json = JSON(value)
-                    print("JSON: \(json)")
-                    let drwtNo1 = json["drwtNo1"].intValue
-                    let drwtNo2 = json["drwtNo2"].intValue
-                    let drwtNo3 = json["drwtNo3"].intValue
-                    let drwtNo4 = json["drwtNo4"].intValue
-                    let drwtNo5 = json["drwtNo5"].intValue
-                    let drwtNo6 = json["drwtNo6"].intValue
-                    let bnusNo = json["bnusNo"].intValue
-                    
-                    let date = json["drwNoDate"].stringValue
-                    self.drwDateLabel.text = "\(date) 추첨"
-                    
-                    self.result1Label.text = "\(drwtNo1)"
-                    self.result2Label.text = "\(drwtNo2)"
-                    self.result3Label.text = "\(drwtNo3)"
-                    self.result4Label.text = "\(drwtNo4)"
-                    self.result5Label.text = "\(drwtNo5)"
-                    self.result6Label.text = "\(drwtNo6)"
-                    
-                    self.resultBonusLabel.text = "\(bnusNo)"
-                    
-                    
-                case .failure(let error):
-                    print(error)
+            self.result1Label.text = "\(savedArray[0])"
+            self.result2Label.text = "\(savedArray[1])"
+            self.result3Label.text = "\(savedArray[2])"
+            self.result4Label.text = "\(savedArray[3])"
+            self.result5Label.text = "\(savedArray[4])"
+            self.result6Label.text = "\(savedArray[5])"
+            self.resultBonusLabel.text = "\(savedArray[6])"
+            self.drwDateLabel.text = "\(savedArray[7]) 추첨"
+            
+            
+        } else {
+            // 저장된 값이 아니라면(새로 불러온다면)
+            let url = "https://www.dhlottery.co.kr/common.do?method=getLottoNumber&drwNo=\(drwNo)"
+            
+                // https://www.dhlottery.co.kr/common.do? method=getLottoNumber&drwNo=903
+                
+                AF.request(url, method: .get).validate().responseJSON { response in
+                    switch response.result {
+                    case .success(let value):
+                        let json = JSON(value)
+                       
+                        let drwtNo1 = json["drwtNo1"].intValue
+                        let drwtNo2 = json["drwtNo2"].intValue
+                        let drwtNo3 = json["drwtNo3"].intValue
+                        let drwtNo4 = json["drwtNo4"].intValue
+                        let drwtNo5 = json["drwtNo5"].intValue
+                        let drwtNo6 = json["drwtNo6"].intValue
+                        let bnusNo = json["bnusNo"].intValue
+                        
+                        let date = json["drwNoDate"].stringValue
+                        let drwtNumbers: [Any] = [drwtNo1, drwtNo2, drwtNo3, drwtNo4, drwtNo5, drwtNo6, bnusNo, date]
+                        UserDefaults.standard.set(drwtNumbers, forKey: drwNo)
+                        print(drwtNumbers)
+                        self.drwDateLabel.text = "\(date) 추첨"
+                        
+                        self.result1Label.text = "\(drwtNo1)"
+                        self.result2Label.text = "\(drwtNo2)"
+                        self.result3Label.text = "\(drwtNo3)"
+                        self.result4Label.text = "\(drwtNo4)"
+                        self.result5Label.text = "\(drwtNo5)"
+                        self.result6Label.text = "\(drwtNo6)"
+                        
+                        self.resultBonusLabel.text = "\(bnusNo)"
+                        
+                        
+                    case .failure(let error):
+                        print(error)
+                }
             }
+            
+            
         }
         
+            
+        
+        
+    }
+    
+    func checkDrwNo(drwNo: String) -> Bool {
+        if let result = UserDefaults.standard.array(forKey: drwNo) {
+            self.savedArray = result
+            return true
+        } else {
+            return false
+        }
+        
+
     }
     
     func setLabelUI(label: UILabel) {
