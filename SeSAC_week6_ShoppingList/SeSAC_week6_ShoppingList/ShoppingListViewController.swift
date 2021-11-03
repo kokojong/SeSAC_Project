93f1685454f14fe7b786333c504ceef47b226436
kokojong <duwhd0349@kookmin.ac.kr>
2021-11-03 23:03:53 +0900

    11.3(wed) 쇼핑리스트 프로젝트 - 북마크, 체크 버튼 동작, alertAction으로 정렬 추가

--- SeSAC_week6_ShoppingList/SeSAC_week6_ShoppingList/ShoppingListViewController.swift
+++ SeSAC_week6_ShoppingList/SeSAC_week6_ShoppingList/ShoppingListViewController.swift
@@ -14,7 +14,7 @@ class ShoppingListViewController: UIViewController {
     
     @IBOutlet weak var shoppingTextField: UITextField!
     
-    var shoppingList: Results<ShoppingList>!
+    var tasks: Results<ShoppingList>!
     
     let localRealm = try! Realm()
     
@@ -26,7 +26,7 @@ class ShoppingListViewController: UIViewController {
         
         print("Realm:",localRealm.configuration.fileURL!)
         
-        shoppingList = localRealm.objects(ShoppingList.self)
+        tasks = localRealm.objects(ShoppingList.self)
         
     }
     
@@ -37,6 +37,7 @@ class ShoppingListViewController: UIViewController {
     @IBAction func onAddButtonClicked(_ sender: UIButton) {
         
         let task = ShoppingList(itemName: shoppingTextField.text!)
+        
         try! localRealm.write {
             localRealm.add(task)
         }
@@ -45,12 +46,48 @@ class ShoppingListViewController: UIViewController {
         
     }
     
+    @IBAction func onSortButtonClicked(_ sender: UIButton) {
+        
+        // 1. UIAlertController 생성 : 밑바탕 + 타이틀 + 본문
+        let alert = UIAlertController(title: "정렬하기", message: "아래의 정렬 방식 중 한 가지를 골라주세요", preferredStyle: .actionSheet)
+        // 2. UIAlertAction 생성 : 버튼들을 만들어준다
+        let check = UIAlertAction(title: "완료된 순으로 정렬", style: .default) { action in
+            let sortedByCheck = self.tasks.sorted(byKeyPath: "check", ascending: false)
+            self.tasks = sortedByCheck
+           
+            self.shoppingTableView.reloadData()
+        }
+        
+        let item = UIAlertAction(title: "이름 순(오름차순)으로 정렬", style: .default) { action in
+            let sortedByName = self.tasks.sorted(byKeyPath: "itemName", ascending: true)
+            self.tasks = sortedByName
+            
+            self.shoppingTableView.reloadData()
+        }
+        let bookmark = UIAlertAction(title: "즐겨찾기 순으로 정렬", style: .default) { action in
+            let sortedBybookmark = self.tasks.sorted(byKeyPath: "bookmark", ascending: false)
+            self.tasks = sortedBybookmark
+            
+            self.shoppingTableView.reloadData()
+        }
+        
+        // 3. 1과 2를 합쳐준다
+        // addAction의 순서대로 버튼이 붙는다
+        alert.addAction(check)
+        alert.addAction(item)
+        alert.addAction(bookmark)
+        
+        // 4. Present (보여줌) - modal처럼
+        present(alert, animated: true, completion: nil)
+        
+    }
+    
 }
 
 extension ShoppingListViewController: UITableViewDelegate, UITableViewDataSource {
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return shoppingList.count
+        return tasks.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
@@ -59,7 +96,7 @@ extension ShoppingListViewController: UITableViewDelegate, UITableViewDataSource
             return UITableViewCell()
         }
         
-        let row = shoppingList[indexPath.row]
+        let row = tasks[indexPath.row]
         
         cell.shoppingLabel.text = row.itemName
         
@@ -91,7 +128,7 @@ extension ShoppingListViewController: UITableViewDelegate, UITableViewDataSource
         print("check")
         
         // All modifications to a realm must happen in a write block.
-        let taskToUpdate = shoppingList[selected.tag]
+        let taskToUpdate = tasks[selected.tag]
         try! localRealm.write {
             taskToUpdate.check = (taskToUpdate.check + 1) % 2
         }
@@ -102,7 +139,7 @@ extension ShoppingListViewController: UITableViewDelegate, UITableViewDataSource
     @objc func onBookmarkButtonClicked(selected: UIButton) {
         print("bookmark")
         
-        let taskToUpdate = shoppingList[selected.tag]
+        let taskToUpdate = tasks[selected.tag]
         try! localRealm.write {
             taskToUpdate.bookmark = (taskToUpdate.bookmark + 1) % 2
         }
@@ -110,9 +147,24 @@ extension ShoppingListViewController: UITableViewDelegate, UITableViewDataSource
         
     }
     
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 60
+    }
     
     
+    func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
+        return true
+    }
     
-    
-    
+    func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
+        
+        try! localRealm.write {
+            
+            localRealm.delete(tasks[indexPath.row])
+            tableView.reloadData()
+            
+        }
+        
+    }
+
 }
